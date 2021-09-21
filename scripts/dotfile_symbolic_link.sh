9c025542706520d43cfddc2f053fbed12fc461b1
yoshiki <yoshiki0911@gmail.com>
2021-09-22 01:35:10 +0900

    update global gitignore file

--- scripts/dotfile_symbolic_link.sh
+++ scripts/dotfile_symbolic_link.sh
@@ -6,11 +6,20 @@ for dotfile in .??*; do
     ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
 done
 
-# create .config in $HOME
+echo "\n========================"
+echo "create .config in $HOME"
 mkdir -p $HOME/.config
-for dotfile in "$(ls .config)"; do
+for dotfile in $(ls -F .config | grep -v /); do
     [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
     ln -snfv "$(pwd)/.config/$dotfile" "$HOME/.config/$dotfile"
+done
+
+echo "\n================================="
+echo "create .config/git in $HOME"
+mkdir -p $HOME/.config/git
+for dotfile in $(ls -F .config/git | grep -v /); do
+    [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+    ln -snfv "$(pwd)/.config/git/$dotfile" "$HOME/.config/git/$dotfile"
 done
 
 echo "Success"
