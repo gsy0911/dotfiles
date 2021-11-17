dfb80c56f4ed226f0358f3b60ab07b5bba6f0e9c
gsy0911 <yoshiki0911@gmail.com>
2021-11-17 18:36:52 +0900

    add new brew packages

--- scripts/brew_packages.sh
+++ scripts/brew_packages.sh
@@ -15,6 +15,7 @@ check_exists_then_install jq
 check_exists_then_install peco
 check_exists_then_install starship
 check_exists_then_install colordiff
+check_exists_then_install tokei
 # rust
 check_exists_then_install rustup
 check_exists_then_install rust-analyzer
@@ -25,3 +26,10 @@ check_exists_then_install awscli
 check_exists_then_install node@16
 # python
 check_exists_then_install poetry
+## for scipy
+check_exists_then_install lapack
+## for matplotlib
+check_exists_then_install libjpeg
+## for psycopg2
+check_exists_then_install postgres
+
