a3b126d88e2b3ccfbe68e206c542208bb08be696
gsy0911 <yoshiki0911@gmail.com>
2024-09-25 13:19:03 +0900

    updat WezTerm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,11 @@ local wezterm = require 'wezterm'
 -- This table will hold the configuration.
 local config = wezterm.config_builder()
 
+
+----------------------------------------------------
+-- Window
+----------------------------------------------------
+
 config.color_scheme = 'Wombat'
 -- config.color_scheme = 'Tokyo Night'
 -- config.color_scheme = 'Breeze'
@@ -11,7 +16,8 @@ config.color_scheme = 'Wombat'
 -- フォントサイズ
 config.font_size = 13.0
 -- 背景の非透過率（1なら完全に透過させない）
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.70
+config.macos_window_background_blur = 20
 
 config.default_cursor_style = 'BlinkingBar'
 config.cursor_blink_rate = 1000
@@ -31,7 +37,60 @@ config.window_frame = {
 }
 
 
+----------------------------------------------------
+-- Tab
+----------------------------------------------------
+-- タイトルバーを非表示
+config.window_decorations = "RESIZE"
+-- タブの追加ボタンを非表示
+config.show_new_tab_button_in_tab_bar = false
+-- タブが一つしかない時に非表示
+config.hide_tab_bar_if_only_one_tab = true
+-- nightlyのみ使用可能
+-- タブの閉じるボタンを非表示
+-- config.show_close_tab_button_in_tabs = false
+
+-- タブ同士の境界線を非表示
+config.colors = {
+  tab_bar = {
+    inactive_tab_edge = "none",
+  },
+}
+
+
+-- タブの形をカスタマイズ
+-- タブの左側の装飾
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+-- タブの右側の装飾
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
+  local edge_background = "none"
+  if tab.is_active then
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
+  end
+  local edge_foreground = background
+  local title = "  " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "(" .. tab.active_pane.pane_id .. ")  "
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
+
+----------------------------------------------------
 -- Key Bindings
+----------------------------------------------------
+
 local act = wezterm.action
 
 config.keys = {
