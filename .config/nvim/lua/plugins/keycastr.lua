86f5f3262fc13b490e1273f848eeb83338368a7a
gsy0911 <yoshiki0911@gmail.com>
2024-10-10 12:16:46 +0900

    add keycastr

---
+++ .config/nvim/lua/plugins/keycastr.lua
@@ -0,0 +1,15 @@
+return {
+  "4513ECHO/nvim-keycastr",
+  config = function()
+    local keycastr = require("keycastr")
+    keycastr.config.set {
+      ignore_mouse = true,
+      position = "SE",
+      win_config = {
+        border = "rounded",
+      }
+    }
+    keycastr.enable()
+  end,
+}
+
