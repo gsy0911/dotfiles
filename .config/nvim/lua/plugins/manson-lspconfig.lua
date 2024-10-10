aeb2726bcc4bcbc98c6dd12ca3bfc40ea50e735c
gsy0911 <yoshiki0911@gmail.com>
2024-10-10 09:41:50 +0900

    update mason

--- .config/nvim/lua/plugins/manson-lspconfig.lua
+++ .config/nvim/lua/plugins/manson-lspconfig.lua
@@ -3,8 +3,43 @@ return {
   version = "*",
   lazy = false,
   config = function()
+    local lsp_servers = { "lua_ls", "pyright", "ruff", "ts_ls", "html", "yamlls", "jsonls" }
     require("mason-lspconfig").setup {
-      ensure_installed = { "lua_ls" },
+      ensure_installed = lsp_servers,
     }
+    require("mason-lspconfig").setup_handlers {
+      function (server_name)
+        local nvim_lsp = require("lspconfig")
+        require("lspconfig").pyright.setup {
+          root_dir = nvim_lsp.util.root_pattern(".venv"),
+          -- cmd = { "bash", "-c", "source ./.venv/bin/activate"},
+          settins = {
+            python = {
+              -- 仮想環境のルートパス
+              venvPath = ".",
+              -- 仮想環境のフォルダ名
+              -- venv = ".venv",
+              pythonPath = "./.venv/bin/python",
+              -- analysis = {
+              --   extraPaths = {"."},
+              --   autoSearchPaths = true,
+              --   useLibraryCodeForTypes = true
+              -- }
+            }
+          }
+        }
+      end,
+    }
+    -- カーソル下の変数の情報
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+    -- 定義ジャンプ
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+    -- 改行やインデントなどのフォーマット
+    vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+    -- カーソル下の変数をコード内で参照している箇所
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+    -- 変数名のリネーム
+    vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+
   end,
 }
