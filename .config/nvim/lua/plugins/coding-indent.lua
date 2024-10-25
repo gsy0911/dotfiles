e925d95fae32e5f59a6efa2ff15190763b8eddae
gsy0911 <yoshiki0911@gmail.com>
2024-10-25 22:35:23 +0900

    add typescript indent

--- .config/nvim/lua/plugins/coding-indent.lua
+++ .config/nvim/lua/plugins/coding-indent.lua
@@ -6,7 +6,7 @@ return {
   },
   config = function()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = { "python" },
+      ensure_installed = { "python", "typescript" },
       highlight = {
         enable = true,
       }
