90588cd50bd5514cf018c4897319a96d4534eb74
gsy0911 <yoshiki0911@gmail.com>
2024-10-26 16:20:21 +0900

    mofidy diffview

--- .config/nvim/lua/plugins/coding-diffview.lua
+++ .config/nvim/lua/plugins/coding-diffview.lua
@@ -1,8 +1,13 @@
 return {
   "sindrets/diffview.nvim",
+  config = function ()
+    require("diffview").setup()
+  end,
+  lazy = false,
   keys = {
     {mode = "n", "<leader>hh", "<cmd>DiffviewOpen HEAD~1<CR>", desc = "1つ前とのdiff"},
     {mode = "n", "<leader>hf", "<cmd>DiffviewFileHistory %<CR>", desc = "ファイルの変更履歴"},
     {mode = "n", "<leader>hc", "<cmd>DiffviewClose<CR>", desc = "diffの画面閉じる"},
+    {mode = "n", "<leader>hd", "<cmd>Diffview<CR>", desc = "コンフリクト解消画面表示"},
   },
 }
