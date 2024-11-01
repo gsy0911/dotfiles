6d178317f3c21fac2ee02873b686fd197ce110b2
gsy0911 <yoshiki0911@gmail.com>
2024-11-02 00:26:39 +0900

    modify date_format

--- .config/nvim/lua/plugins/coding-gitblame.lua
+++ .config/nvim/lua/plugins/coding-gitblame.lua
@@ -11,7 +11,7 @@ return {
       -- for example
       enabled = true,  -- if you want to enable the plugin
       message_template = " <author>, <date> : <summary>", -- template for the blame message, check the Message template section for more options
-      date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+      date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
       virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
   },
 }
