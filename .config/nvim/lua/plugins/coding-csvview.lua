89937777712972d71f8949a29cc4458d2ab07209
gsy0911 <yoshiki0911@gmail.com>
2024-11-08 09:54:49 +0900

    add csvview

---
+++ .config/nvim/lua/plugins/coding-csvview.lua
@@ -0,0 +1,10 @@
+return {
+  'hat0uma/csvview.nvim',
+  config = function()
+    require('csvview').setup()
+    local csvview = require('csvview')
+    vim.keymap.set('n', '<leader>ct', csvview.toggle, { desc = 'toggle CsvView' })
+    vim.keymap.set('n', '<leader>ww', '<cmd>set wrap<CR>', { desc = 'set wrap' })
+    vim.keymap.set('n', '<leader>wn', '<cmd>set nowrap<CR>', { desc = 'set nowrap' })
+  end
+}
