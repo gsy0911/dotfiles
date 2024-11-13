440bac837f3f16ff3e759154ac5faf44a5bc4276
gsy0911 <yoshiki0911@gmail.com>
2024-11-13 13:40:39 +0900

    modify init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,7 +35,6 @@ vim.keymap.set("n", "<space>say", function()
   vim.api.nvim_feedkeys(":!say -v Ava " .. current_word .. "\n", "n", false)
 end, { desc = "say command" })
 
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -57,29 +56,7 @@ vim.opt.rtp:prepend(lazypath)
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"-- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
--- see: https://github.com/folke/lazy.nvim
 
 -- Setup lazy.nvim
 require("lazy").setup({
