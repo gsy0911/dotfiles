281785d41d8ab8bce3d0988bcfd5c9115b38932f
gsy0911 <yoshiki0911@gmail.com>
2024-11-18 12:30:13 +0900

    add barbar

--- .config/nvim/lua/plugins/buffer-bufferline.lua
+++ .config/nvim/lua/plugins/buffer-bufferline.lua
@@ -1,6 +1,6 @@
 return {
   'akinsho/bufferline.nvim',
-  version = "*",
+  enabled = false,
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function ()
     vim.opt.termguicolors = true
