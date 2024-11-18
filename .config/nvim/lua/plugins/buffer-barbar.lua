0f87398d83a98c3ee61aa10f469e03a0be66bba0
gsy0911 <yoshiki0911@gmail.com>
2024-11-18 19:19:33 +0900

    modify barbar

--- .config/nvim/lua/plugins/buffer-barbar.lua
+++ .config/nvim/lua/plugins/buffer-barbar.lua
@@ -4,11 +4,16 @@ return {
     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
-  init = function() vim.g.barbar_auto_setup = false end,
+  lazy = false,
+  -- init = function() vim.g.barbar_auto_setup = false end,
   opts = {
     -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
     -- animation = true,
     -- insert_at_start = true,
     -- …etc.
+  },
+  keys = {
+    {mode = "n", "<C-b>a", "<cmd>BufferCloseAllButCurrent<CR>", desc = "開いているbuffer以外を閉じる"},
+    {mode = "n", "<C-b>c", "<cmd>BufferClose<CR>", desc = "bufferを閉じる"},
   },
 }
