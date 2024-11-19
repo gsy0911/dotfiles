58ab1c341925e0a016db428e69e3c57787a432ee
gsy0911 <yoshiki0911@gmail.com>
2024-11-19 13:40:53 +0900

    add git modified lines

--- .config/nvim/lua/plugins/coding-minimap.lua
+++ .config/nvim/lua/plugins/coding-minimap.lua
@@ -4,8 +4,26 @@ return {
   -- enabled = false,
   lazy = false,
   config = function()
-    require('mini.map').setup()
-    local minimap = require('mini.map')
+   require('mini.diff').setup()
+   local minimap = require('mini.map')
+    -- :h MiniMap.gen_integration
+    -- local hl_groups = {
+    --   add = "GitSignsAdd",       -- 追加された行に対するハイライトグループ
+    --   change = "GitSignsChange", -- 変更された行に対するハイライトグループ
+    --   delete = "GitSignsDelete", -- 削除された行に対するハイライトグループ
+    -- }
+    -- local gitsing_integration = minimap.gen_integration.gitsigns(hl_groups)
+    
+    local hl_groups = {
+      add = "MiniDiffSignAdd",       -- 追加された行に対するハイライトグループ
+      change = "MiniDiffSignChange", -- 変更された行に対するハイライトグループ
+      delete = "MiniDiffDelete", -- 削除された行に対するハイライトグループ
+    }
+    local integration = minimap.gen_integration.diff(hl_groups)
+
+    minimap.setup({
+      integrations = { integration }
+    })
     minimap.open()
   end
 } 
