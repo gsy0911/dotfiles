c46eecd8bfe4b832deba2b3c06342fbdea705352
gsy0911 <yoshiki0911@gmail.com>
2024-11-26 16:40:17 +0900

    modify telescope

--- .config/nvim/lua/plugins/fzf-telescope.lua
+++ .config/nvim/lua/plugins/fzf-telescope.lua
@@ -2,6 +2,26 @@ return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup({
+      defaults = {
+        -- find_files = { "fd", "--type", "f", "--hidden", "--exclude", ".git"},
+        find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*"},
+        -- find_files = { "rg", "--files", "--hidden", "--glob", "!.git/*"},
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden', -- 隠しファイルを含む
+          '--glob',
+          '!.git/*',
+        }
+
+      }
+    })
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
