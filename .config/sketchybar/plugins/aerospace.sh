9205840d7eb8f5c77b1e885a25f17fa5da032e80
gsy0911 <yoshiki0911@gmail.com>
2025-01-03 23:29:26 +0900

    at least working

---
+++ .config/sketchybar/plugins/aerospace.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SPACE_ID="$1"
+FOCUSED_WORKSPACE="$"
+
+if [[ "$SPACE_ID" == "$FOCUSED_WORKSPACE" ]]; then
+    sketchybar --set space.$SPACE_ID icon.color=0xff89a4fb
+else
+    sketchybar --set space.$SPACE_ID icon.color=0xfff38ba8
+fi
