c91c2f31e8319f2eb393105771d262c993ca3bd9
gsy0911 <yoshiki0911@gmail.com>
2025-01-04 12:08:09 +0900

    modify chmod

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SPACE_ID="$1"
-FOCUSED_WORKSPACE="$"
+FOCUSED_WORKSPACE="$AEROSPACE_FOCUSED_WORKSPACE"
 
 if [[ "$SPACE_ID" == "$FOCUSED_WORKSPACE" ]]; then
     sketchybar --set space.$SPACE_ID icon.color=0xff89a4fb
