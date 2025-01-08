4608a5a5db001a629c64fdd001dd01885ffa6e57
gsy0911 <yoshiki0911@gmail.com>
2025-01-09 08:49:42 +0900

    add codecompanion

--- .config/nvim/lua/plugins/coding-llm-codecompanion.lua
+++ .config/nvim/lua/plugins/coding-llm-codecompanion.lua
@@ -4,5 +4,25 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  config = true
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "anthropic",
+        },
+        inline = {
+          adapter = "anthropic",
+        },
+      },
+      adapters = {
+        anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "ANTHROPIC_API_KEY"
+            },
+          })
+        end,
+      },
+    })
+  end
 }
