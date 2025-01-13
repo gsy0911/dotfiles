dfc8a7dbec3faf37683e61419287edeb49676211
gsy0911 <yoshiki0911@gmail.com>
2025-01-13 16:54:42 +0900

    configの修正

--- src/dotfiles/generate_mcp_config.py
+++ src/dotfiles/generate_mcp_config.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     config = generate_mcp_config()
     path = f"{os.environ['HOME']}/Development/Projects/dotfiles/.config/mcp/claude_desktop_config.json"
     with open(path, mode="w") as f:
-        json.dump(config, f)
-    print(f"{config=}")
+        json.dump(config, f, indent=2)
+
