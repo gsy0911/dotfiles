2794d160f69abb945de32e5bf980c94528352519
gsy0911 <yoshiki0911@gmail.com>
2025-01-22 12:30:35 +0900

    make it functions

--- .config/sketchybar/plugins/aerospace_app.sh
+++ .config/sketchybar/plugins/aerospace_app.sh
@@ -4,18 +4,23 @@
 # variable. E.g. the front_app_switched event sends the name of the newly
 # focused application in the $INFO variable:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+function workspace() {
+  if [[ $1 ]]; then
+    WINDOW="$(aerospace list-workspaces --monitor $1 --visible)"
+    WINDOW_NON="$(aerospace list-workspaces --monitor $1 --visible no --empty no | paste -s -d ',' -)"
+    LABEL="$WINDOW($WINDOW_NON)"
+    echo $LABEL
+  fi
+}
 
 if [ "$SENDER" = "front_app_switched" ]; then
   MONITOR_NUM="$(aerospace list-monitors --count)"
-  WINDOW1="$(aerospace list-workspaces --monitor 1 --visible)"
-  WINDOW1_NON="$(aerospace list-workspaces --monitor 1 --visible no --empty no | paste -s -d ',' -)"
+  WINDOW1="$(workspace 1)"
   LABEL="$WINDOW1($WINDOW1_NON)"
   if [ "$MONITOR_NUM" = "3" ]; then
-    WINDOW2="$(aerospace list-workspaces --monitor 2 --visible)"
-    WINDOW2_NON="$(aerospace list-workspaces --monitor 2 --visible no --empty no | paste -s -d ',' -)"
-    WINDOW3="$(aerospace list-workspaces --monitor 3 --visible)"
-    WINDOW3_NON="$(aerospace list-workspaces --monitor 3 --visible no --empty no | paste -s -d ',' -)"
-    LABEL="$WINDOW1($WINDOW1_NON) | $WINDOW2($WINDOW2_NON) | $WINDOW3($WINDOW3_NON)"
+    WINDOW2="$(workspace 2)"
+    WINDOW3="$(workspace 3)"
+    LABEL="$WINDOW1 | $WINDOW2 | $WINDOW3"
   fi
   
   sketchybar --set aerospace_app label="$LABEL"
