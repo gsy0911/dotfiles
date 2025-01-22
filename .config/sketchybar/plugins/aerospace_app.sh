f355b3744ceda6fcbb1ae48a8b936c714f6d6704
gsy0911 <yoshiki0911@gmail.com>
2025-01-22 12:49:25 +0900

    modify functions

--- .config/sketchybar/plugins/aerospace_app.sh
+++ .config/sketchybar/plugins/aerospace_app.sh
@@ -4,12 +4,38 @@
 # variable. E.g. the front_app_switched event sends the name of the newly
 # focused application in the $INFO variable:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
-function workspace() {
+
+function window() {
+  # $1 = workspace-id
   if [[ $1 ]]; then
-    WINDOW="$(aerospace list-workspaces --monitor $1 --visible)"
-    WINDOW_NON="$(aerospace list-workspaces --monitor $1 --visible no --empty no | paste -s -d ',' -)"
-    LABEL="$WINDOW($WINDOW_NON)"
-    echo $LABEL
+    if [[ "$1" -ge 1 && "$1" -le 9 ]]; then
+      local window="$(aerospace list-windows --workspace "$1" --format "%{app-name}" | uniq | paste -s -d "," -)"
+      echo "$1:$window"
+    else
+     echo "$1"
+    fi
+  fi
+}
+
+function listWindowsVisibleNo() {
+  # $1 = monitor-id
+  if [[ $1 ]]; then
+    local workspaces="$(aerospace list-workspaces --monitor $1 --visible no --empty no)"
+    for workspaceId in $workspaces
+    do
+      echo "$(window "$workspaceId")"
+    done
+  fi
+}
+
+function workspace() {
+  # $1 = monitor-id
+  if [[ $1 ]]; then
+    local workspace="$(aerospace list-workspaces --monitor $1 --visible)"
+    local workspace_non="$(aerospace list-workspaces --monitor $1 --visible no --empty no | paste -s -d ',' -)"
+    local workspaceWithName="$(listWindowsVisibleNo $1 | paste -s -d ',' -)"
+    local label="$workspace ($workspaceWithName)"
+    echo $label
   fi
 }
 
