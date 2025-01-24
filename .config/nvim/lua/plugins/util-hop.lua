e8140facd54ddf460247693c05a9232cd5eb9cf5
gsy0911 <yoshiki0911@gmail.com>
2025-01-24 10:10:32 +0900

    add hop

---
+++ .config/nvim/lua/plugins/util-hop.lua
@@ -0,0 +1,11 @@
+return {
+  'smoka7/hop.nvim',
+  version = "*",
+  keys = {
+    {mode = "n", "<leader>hw", "<cmd>HopWord<CR>", desc = "HopWord"},
+    {mode = "n", "<leader>hl", "<cmd>HopLine<CR>", desc = "HopWord"},
+  },
+  opts = {
+      keys = 'etovxqpdygfblzhckisuran'
+  }
+}
