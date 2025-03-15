639029724a4b73c025eb038aa901b9742c437c52
gsy0911 <yoshiki0911@gmail.com>
2025-03-15 23:10:22 +0900

    add new-coding-indent

--- .config/nvim/lua/plugins/coding-indent.lua
+++ .config/nvim/lua/plugins/coding-indent.lua
@@ -1,6 +1,7 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
   event = "VeryLazy",
+  enabled = false,
   dependencies = {
     'HiPhish/rainbow-delimiters.nvim',
     'nvim-treesitter/nvim-treesitter',
