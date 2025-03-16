2193eb3b396dbcde72ce5d7e648ea7e4ff1fcc15
gsy0911 <yoshiki0911@gmail.com>
2025-03-16 12:55:34 +0900

    modify line_num and indent

--- .config/nvim/lua/plugins/coding-indent-hlchunk.lua
+++ .config/nvim/lua/plugins/coding-indent-hlchunk.lua
@@ -7,6 +7,28 @@ return {
         enable = true,
         delay = 0,
       },
+      line_num = {
+        enable = true,
+        style = "#806d9c",
+      },
+      indent = {
+        enable = true,
+        chars = {
+          "│",
+        },
+        style = {
+          vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui"),
+        },
+        -- style = {
+        --   "#FF0000",
+        --   "#FF7F00",
+        --   "#FFFF00",
+        --   "#00FF00",
+        --   "#00FFFF",
+        --   "#0000FF",
+        --   "#8B00FF",
+        -- },
+      }
     })
   end
 }
