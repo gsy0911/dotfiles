84ca4371197f0a9fc8963bc81e13142ad45fb003
gsy0911 <yoshiki0911@gmail.com>
2025-05-02 00:47:43 +0900

    add mcp-lambda

--- src/dotfiles/domain/mcp_server.py
+++ src/dotfiles/domain/mcp_server.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, model_serializer
+import os
 
 
 class McpServerConfig(BaseModel):
@@ -7,6 +8,7 @@ class McpServerConfig(BaseModel):
     args: list[str]
     url: str
     enabled: bool
+    env: dict
 
     @staticmethod
     def generate() -> list["McpServerConfig"]:
@@ -15,6 +17,7 @@ class McpServerConfig(BaseModel):
             McpServerConfig._jetbrains(),
             McpServerConfig._aws_mcp(),
             McpServerConfig._aws_cdk_mcp(),
+            McpServerConfig._aws_lambda_mcp(),
         ]
 
     @staticmethod
@@ -29,6 +32,7 @@ class McpServerConfig(BaseModel):
             ],
             url="",
             enabled=True,
+            env={},
         )
 
     @staticmethod
@@ -42,6 +46,7 @@ class McpServerConfig(BaseModel):
             ],
             url="https://github.com/JetBrains/mcp-jetbrains?tab=readme-ov-file",
             enabled=False,
+            env={},
         )
 
     @staticmethod
@@ -49,11 +54,10 @@ class McpServerConfig(BaseModel):
         return McpServerConfig(
             name="aws-docs",
             command="uvx",
-            args=[
-                "awslabs.aws-documentation-mcp-server@latest"
-            ],
+            args=["awslabs.aws-documentation-mcp-server@latest"],
             url="https://github.com/awslabs/mcp",
             enabled=True,
+            env={},
         )
 
     @staticmethod
@@ -61,13 +65,27 @@ class McpServerConfig(BaseModel):
         return McpServerConfig(
             name="aws-cdk",
             command="uvx",
-            args=[
-                "awslabs.cdk-mcp-server@latest"
-            ],
+            args=["awslabs.cdk-mcp-server@latest"],
             url="https://github.com/awslabs/mcp",
             enabled=True,
+            env={},
+        )
+
+    @staticmethod
+    def _aws_lambda_mcp() -> "McpServerConfig":
+        return McpServerConfig(
+            name="aws-lambda",
+            command="uvx",
+            args=["awslabs.lambda-mcp-server@latest"],
+            url="https://github.com/awslabs/mcp",
+            enabled=True,
+            env={
+                "AWS_PROFILE": os.environ["AWS_PROFILE"],
+                "AWS_REGION": "ap-northeast-1",
+                "FUNCTION_LIST": "gsy0911-mcp-server",
+            },
         )
 
     @model_serializer
     def serialise(self) -> dict:
-        return {f"{self.name}": {"command": self.command, "args": self.args}}
+        return {f"{self.name}": {"command": self.command, "args": self.args, "env": self.env}}
