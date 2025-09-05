98207b0757f86f00bf46876deaf765af7c06b6ef
gsy0911 <yoshiki0911@gmail.com>
2025-09-05 15:39:35 +0900

    initial build

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -6,10 +6,7 @@
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
+    packages.aarch64-darwin.default = self.packages.aarch64-darwin.hello;
   };
 }
