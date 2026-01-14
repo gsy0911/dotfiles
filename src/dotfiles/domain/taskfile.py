2bff0ab4490d6460fbf66835254b5254014f7a12
gsy0911 <yoshiki0911@gmail.com>
2026-01-14 09:17:59 +0900

    add Taskfile

---
+++ src/dotfiles/domain/taskfile.py
@@ -0,0 +1,65 @@
+import yaml
+from pydantic import BaseModel
+from pathlib import Path
+
+
+class Include(BaseModel):
+    prefix: str
+    taskfile: str
+
+
+class Task(BaseModel):
+    desc: str
+    prefix: str | None
+    name: str
+    requires: dict
+
+    def gen_command(self) -> str:
+        if self.prefix:
+            return f"{self.prefix}:{self.name}"
+        else:
+            return self.name
+
+class Taskfile(BaseModel):
+    includes: list[Include]
+    tasks: list[Task]
+
+
+class TaskFileRepository:
+
+    def __init__(self, path: str, prefix: str | None = None):
+        self.path = Path(path)
+        self.prefix = prefix
+
+    def _read(self) -> Taskfile:
+        with open(self.path, 'r', encoding='utf-8') as f:
+            docs = list(yaml.safe_load_all(f))
+
+        includes = []
+        for k, v in docs[0].get("includes", {}).items():
+            if type(v) == str:
+                i = Include(prefix=k, taskfile=v)
+                includes.append(i)
+            elif type(v) == dict:
+                i = Include(prefix=k, taskfile=v.get("taskfile", ""))
+                includes.append(i)
+
+        tasks = []
+        for k, v in docs[0]["tasks"].items():
+            t = Task(prefix=self.prefix, name=k, desc=v.get("desc", ""), requires=v.get("requires", {}))
+            tasks.append(t)
+        return Taskfile(includes=includes, tasks=tasks)
+
+    def read_tasks(self) -> list[Task]:
+        base_taskfile = self._read()
+        tasks = base_taskfile.tasks
+        for i in base_taskfile.includes:
+            if i.taskfile.startswith("https://"):
+                pass
+            else:
+                relative_path = Path(i.taskfile)
+                target_path = self.path.parent / relative_path
+                tasks.extend(TaskFileRepository(path=target_path, prefix=i.prefix)._read().tasks)
+                print(target_path)
+
+        return tasks
