b8588e31ce819035e3f95d5e60dee5e2866fbe61
gsy0911 <yoshiki0911@gmail.com>
2026-01-20 18:56:22 +0900

    add homebrew-cask

--- nix/nix-darwin/homebrew.nix
+++ nix/nix-darwin/homebrew.nix
@@ -13,30 +13,47 @@
     # };
     brews = [
       ### CLI Applications not available in nixpkgs
-      # "fisher"
       "htop"
     ];
     taps = [
-      # "nikitabobko/tap"
+      "nikitabobko/tap"
     ];
     casks = [
       ### GUI Applications
-      # "wezterm"
-      # "1password"
-      # "1password-cli"
-      "aerospace"
-      "cursor"
+      "font-cica"
+      # icons for sketchybar
+      "font-hack-nerd-font"
+      "sf-symbols"
+      # claude
+      "claude"
+      # "claude-code"
+      # # VNC
+      # - vnc-viewer
+      # terminal
+      "ghostty"
+      "wezterm"
+      # browser
       "firefox"
+      "google-chrome"
+      # note
       "obsidian"
-      # "raycast"
-      # "stats"
-      # "shottr"
-      # "notchnook"
-      # "jordanbaird-ice"
-      # "kap"
-      # "keycastr"
-      # "zed"
-      # "arc"
+      "notion"
+      # window-manager
+      "aerospace"
+      # IDE
+      "jetbrains-toolbox"
+      "visual-studio-code"
+      "cursor"
+      # Git
+      "fork"
+      # development
+      "aws-vault"
+      "docker"
+      # other
+      "adobe-acrobat-reader"
+      "insomnia"
+      "vlc"
+      "minecraft"
 
       ### Fonts
       # "font-google-sans-code"
