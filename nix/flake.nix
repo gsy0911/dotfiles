e60d6b8f9044f07241ae0173303bacd305608096
gsy0911 <yoshiki0911@gmail.com>
2026-01-20 09:04:15 +0900

    modify output

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,14 +5,30 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.aarch64-darwin.my-packages = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
-      name = "my-packages-list";
-      paths = [
-        nixpkgs.legacyPackages.aarch64-darwin.git
-        nixpkgs.legacyPackages.aarch64-darwin.curl
-        # ここにパッケージを追記していく
-      ];
-    };
+  outputs = { self, nixpkgs }:
+    let
+       system = "aarch64-darwin";
+       pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.my-packages = pkgs.buildEnv {
+        name = "my-packages-list";
+        paths = [
+          pkgs.git
+          pkgs.curl
+          # ここにパッケージを追記していく
+        ];
+      };
+       apps.${system}.update = {
+         type = "app";
+         program = toString (pkgs.writeShellScript "update-script" ''
+         set -e
+         echo "Updating flake..."
+         nix flake update
+         echo "Updating profile..."
+         nix profile upgrade my-packages
+         echo "Update complete!"
+       '');
+       };
   };
 }
