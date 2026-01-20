43701b06a96ed3b4c32614a54788ea95254d41f3
gsy0911 <yoshiki0911@gmail.com>
2026-01-20 09:20:45 +0900

    modify homeConfig

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,10 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
-    let
+
+
+  outputs = { self, nixpkgs, home-manager } @ inputs: let
        system = "aarch64-darwin";
        pkgs = nixpkgs.legacyPackages.${system};
     in
@@ -19,16 +24,29 @@
           # ここにパッケージを追記していく
         ];
       };
-       apps.${system}.update = {
-         type = "app";
-         program = toString (pkgs.writeShellScript "update-script" ''
-         set -e
-         echo "Updating flake..."
-         nix flake update
-         echo "Updating profile..."
-         nix profile upgrade my-packages
-         echo "Update complete!"
-       '');
-       };
+      apps.${system}.update = {
+        type = "app";
+        program = toString (pkgs.writeShellScript "update-script" ''
+        set -e
+        echo "Updating flake..."
+        nix flake update
+        echo "Updating home-manager..."
+        nix run nixpkgs#home-manager -- switch --flake .#myHomeConfig
+        echo "Update complete!"
+      '');
+      };
+      homeConfigurations = {
+      myHomeConfig = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = system;
+        };
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
   };
 }
