1c8829a0d60acb28fff668434e04919d89c25aa0
gsy0911 <yoshiki0911@gmail.com>
2026-04-29 12:00:28 +0900

    modify zsh

--- nix/nix-darwin/config.nix
+++ nix/nix-darwin/config.nix
@@ -3,4 +3,19 @@
     ./system.nix
     ./homebrew.nix
   ];
+
+  # nix-darwin が /etc/zshrc に自動挿入する `promptinit` を無効化する。
+  # promptinit は fpath 内の全 prompt_*_setup ファイルを -r でスキャンするため、
+  # /run/current-system 以下の特定ファイルで zsh 起動がハングする原因となる。
+  programs.zsh.promptInit = "";
+
+  # compinit のセキュリティチェック (compaudit) を無効化する。
+  # compaudit は fpath 内の全ディレクトリのパーミッションを走査するため、
+  # /run/current-system 以下のパスで zsh 起動がハングする原因となる。
+  # デフォルトの compinit を無効化し、/run/current-system パスを fpath から除外した上で再定義する。
+  programs.zsh.enableCompletion = false;
+  programs.zsh.interactiveShellInit = ''
+    fpath=(''${fpath:#/run/current-system/*})
+    autoload -U compinit && compinit -C
+  '';
 }
